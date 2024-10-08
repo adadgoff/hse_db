80d0424bc7c366490e4a9580cd60fc02eb3d7621
superart04 <adadgoff@mail.ru>
2024-10-08 13:36:10 +0300

    change folders & init solution task 5

---
+++ task05/subtask01/init_tables.sql
@@ -0,0 +1,54 @@
+CREATE TABLE Reader (
+    ID SERIAL PRIMARY KEY,
+    LastName VARCHAR(100) NOT NULL,
+    FirstName VARCHAR(100) NOT NULL,
+    Address VARCHAR(255) NOT NULL,
+    BirthDate DATE NOT NULL
+);
+
+CREATE TABLE Publisher (
+    PubName VARCHAR(100) PRIMARY KEY,
+    PubAddress VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Book (
+    ISBN CHAR(13) PRIMARY KEY,
+    Title VARCHAR(255) NOT NULL,
+    Author VARCHAR(100) NOT NULL,
+    PagesNum INT NOT NULL,
+    PubYear INT NOT NULL,
+    PubName VARCHAR(100),
+    FOREIGN KEY (PubName) REFERENCES Publisher(PubName) ON DELETE SET NULL
+);
+
+CREATE TABLE Category (
+    CategoryName VARCHAR(100) PRIMARY KEY,
+    ParentCat VARCHAR(100),
+    FOREIGN KEY (ParentCat) REFERENCES Category(CategoryName) ON DELETE SET NULL
+);
+
+CREATE TABLE Copy (
+    ISBN CHAR(13),
+    CopyNumber INT,
+    ShelfPosition VARCHAR(50),
+    PRIMARY KEY (ISBN, CopyNumber),
+    FOREIGN KEY (ISBN) REFERENCES Book(ISBN) ON DELETE CASCADE
+);
+
+CREATE TABLE Borrowing (
+    ReaderNr INT,
+    ISBN CHAR(13),
+    CopyNumber INT,
+    ReturnDate DATE,
+    PRIMARY KEY (ReaderNr, ISBN, CopyNumber),
+    FOREIGN KEY (ReaderNr) REFERENCES Reader(ID) ON DELETE CASCADE,
+    FOREIGN KEY (ISBN, CopyNumber) REFERENCES Copy(ISBN, CopyNumber) ON DELETE CASCADE
+);
+
+CREATE TABLE BookCat (
+    ISBN CHAR(13),
+    CategoryName VARCHAR(100),
+    PRIMARY KEY (ISBN, CategoryName),
+    FOREIGN KEY (ISBN) REFERENCES Book(ISBN) ON DELETE CASCADE,
+    FOREIGN KEY (CategoryName) REFERENCES Category(CategoryName) ON DELETE CASCADE
+);
